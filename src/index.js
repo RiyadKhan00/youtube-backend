e3d378a71227beced0b0d7dafea64b95d68178b2
RiyadKhan00 <piyas.khan.j.r.oh@email.com>
2025-07-13 14:12:55 +0600

    util setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import "dotenv/config";
 import connectDb from "./db/index.js";
+import { app } from "./app.js";
 
-connectDb();
+connectDb()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(` Server is running at port : ${process.env.PORT}`);
+    });
+
+    app.on("error", (error) => {
+      console.log("ERR", error);
+    });
+  })
+  .catch((err) => {
+    console.log("MOngo db connection failed !!!", err);
+  });
