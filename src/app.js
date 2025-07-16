154b2933adb72cedb18318664163c6a6d19b99d1
RiyadKhan00 <piyas.khan.j.r.oh@email.com>
2025-07-16 14:30:15 +0600

    all cunfigure done

--- src/app.js
+++ src/app.js
@@ -6,14 +6,21 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.evn.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
+``;
 
 app.use(express.json({ limit: "16kb " }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+//routes decleartion
+app.use("/api/v1/users", userRouter);
 
 export { app };
